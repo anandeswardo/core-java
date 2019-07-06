b62cc74c9a2fad9996c8dde35a706617280d5cfb
anandeswardo <43208752+anandeswardo@users.noreply.github.com>
2019-07-06 08:56:28 +0530

    Create Tableonetoten.java

---
+++ Tableonetoten.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Tableonetoten
+{
+    public static void main(String[] args)
+    {
+        int i,n,prod=0;
+        for(n=1;n<11;n++)
+        {
+            for(i=1;i<11;i++)
+            {
+                prod=n*i;
+                System.out.println(prod);
+            }
+        }
+    }
+}
