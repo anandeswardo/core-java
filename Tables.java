9bb5c08eff4d0e681e8fe58aecee5c09a5f9e060
anandeswardo <43208752+anandeswardo@users.noreply.github.com>
2019-07-06 08:23:53 +0530

    Create Tables.java

---
+++ Tables.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Tables
+{
+    public static void main(String[] args)
+    {
+     int a,i,prod=0;
+     Scanner sc=new Scanner(System.in);
+     a=sc.nextInt();
+     for (i=1;i<11;i++)
+     {
+     prod=a*i;
+     System.out.println(prod);
+    }
+}
+}
